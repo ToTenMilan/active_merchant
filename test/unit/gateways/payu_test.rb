e23c060f9110d3bffa7b6bffeb3a759a99c22d19
Grzegorz Milanowski <grzegorz.milanowski@codesthq.com>
2020-04-09 11:58:05 +0200

    cleanup

--- test/unit/gateways/payu_test.rb
+++ test/unit/gateways/payu_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 class PayuTest < Test::Unit::TestCase
   def setup
   ########################### CHANGE BEFORE PUSH ###################
-    @gateway = PayuGateway.new(merchant_pos_id: '382705', second_key: 'de5c5d9c49b981ae827528ef34ed8034', client_id: '382705', client_secret: 'ac1c82652fe0eac71e091e1070b84d4a')
+    @gateway = PayuGateway.new(merchant_pos_id: '300746', second_key: '13a980d4f851f3d9a1cfc792fb1f5e50', client_id: '300746', client_secret: '2ee86a66e5d97e3fadc400c9f19b065d')
     @credit_card = credit_card('4444333322221111', verification_value: '123', first_name: 'APPROVED', last_name: '')
     @amount = 100
 
